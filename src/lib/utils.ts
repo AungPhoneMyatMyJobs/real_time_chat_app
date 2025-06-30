45d3ad8522aff59d3bf8a67b7fb43ff98ac2ec0f
DaneroArama <www.aviciiguetta@gmail.com>
2025-06-30 10:52:43 +0630

    any type error handling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -67,12 +67,12 @@ export function getUserAvatarColor(userId: string): string {
 }
 
 // Debounce function
-export function debounce<T extends (...args: any[]) => any>(
-  func: T,
-  wait: number
+export function debounce<T extends (...args: unknown[]) => unknown>(
+    func: T,
+    wait: number
 ): (...args: Parameters<T>) => void {
   let timeout: NodeJS.Timeout;
-  
+
   return (...args: Parameters<T>) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), wait);
@@ -80,17 +80,17 @@ export function debounce<T extends (...args: any[]) => any>(
 }
 
 // Throttle function
-export function throttle<T extends (...args: any[]) => any>(
-  func: T,
-  limit: number
+export function throttle<T extends (...args: unknown[]) => unknown>(
+    func: T,
+    limit: number
 ): (...args: Parameters<T>) => void {
   let inThrottle: boolean;
-  
+
   return (...args: Parameters<T>) => {
     if (!inThrottle) {
       func(...args);
       inThrottle = true;
-      setTimeout(() => inThrottle = false, limit);
+      setTimeout(() => (inThrottle = false), limit);
     }
   };
 }
